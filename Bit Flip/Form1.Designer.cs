feb848118ab8f20626c84630de294844ef4137dc
Corey Morehead <cmoreheadno2@gmail.com>
2021-08-12 10:30:17 -0400

    initial commit

---
+++ Bit Flip/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Bit_Flip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 29);
+            this.button1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 1023);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "I";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(210, 31);
+            this.button2.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 1023);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "II";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(405, 31);
+            this.button3.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(152, 1023);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "IV";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(598, 33);
+            this.button4.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(155, 1021);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "VIII";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(797, 33);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(169, 1021);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "XVI";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1007, 31);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(138, 1023);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "XXXII";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1691, 1270);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+
+    }
+}
+
---
+++ Bit Flip/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bit_Flip
+{
+    public partial class Form1 : Form
+    {
+        System.Drawing.Color Red = System.Drawing.Color.Red;
+        System.Drawing.Color AntiqueWhite = System.Drawing.Color.AntiqueWhite;
+
+        public Form1()
+        {
+            InitializeComponent();
+            button2.BackColor = AntiqueWhite;
+            button1.BackColor = AntiqueWhite;
+            button3.BackColor = AntiqueWhite;
+            button4.BackColor = AntiqueWhite;
+            button5.BackColor = AntiqueWhite;
+            button6.BackColor = AntiqueWhite;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.BackColor == AntiqueWhite)
+            {
+                button1.BackColor = Red;
+            }
+            else if (button1.BackColor == Red)
+            {
+                button1.BackColor = AntiqueWhite;
+            }
+            if (button2.BackColor == AntiqueWhite)
+            {
+                button2.BackColor = Red;
+            }
+            else if (button2.BackColor == Red)
+            {
+                button2.BackColor = AntiqueWhite;
+            }
+            if (button3.BackColor == AntiqueWhite)
+            {
+                button3.BackColor = Red;
+            }
+            else if (button3.BackColor == Red)
+            {
+                button3.BackColor = AntiqueWhite;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.BackColor == AntiqueWhite)
+            {
+                button2.BackColor = Red;
+            }
+            else if (button2.BackColor == Red)
+            {
+                button2.BackColor = AntiqueWhite;
+            }
+            if (button3.BackColor == AntiqueWhite)
+            {
+                button3.BackColor = Red;
+            }
+            else if (button3.BackColor == Red)
+            {
+                button3.BackColor = AntiqueWhite;
+            }
+            if (button4.BackColor == AntiqueWhite)
+            {
+                button4.BackColor = Red;
+            }
+            else if (button4.BackColor == Red)
+            {
+                button4.BackColor = AntiqueWhite;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.BackColor == AntiqueWhite)
+            {
+                button3.BackColor = Red;
+            }
+            else if (button3.BackColor == Red)
+            {
+                button3.BackColor = AntiqueWhite;
+            }
+            if (button4.BackColor == AntiqueWhite)
+            {
+                button4.BackColor = Red;
+            }
+            else if (button4.BackColor == Red)
+            {
+                button4.BackColor = AntiqueWhite;
+            }
+            if (button5.BackColor == AntiqueWhite)
+            {
+                button5.BackColor = Red;
+            }
+            else if (button5.BackColor == Red)
+            {
+                button5.BackColor = AntiqueWhite;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button4.BackColor == AntiqueWhite)
+            {
+                button4.BackColor = Red;
+            }
+            else if (button4.BackColor == Red)
+            {
+                button4.BackColor = AntiqueWhite;
+            }
+            if (button5.BackColor == AntiqueWhite)
+            {
+                button5.BackColor = Red;
+            }
+            else if (button5.BackColor == Red)
+            {
+                button5.BackColor = AntiqueWhite;
+            }
+            if (button6.BackColor == AntiqueWhite)
+            {
+                button6.BackColor = Red;
+            }
+            else if (button6.BackColor == Red)
+            {
+                button6.BackColor = AntiqueWhite;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (button5.BackColor == AntiqueWhite)
+            {
+                button5.BackColor = Red;
+            }
+            else if (button5.BackColor == Red)
+            {
+                button5.BackColor = AntiqueWhite;
+            }
+            if (button6.BackColor == AntiqueWhite)
+            {
+                button6.BackColor = Red;
+            }
+            else if (button6.BackColor == Red)
+            {
+                button6.BackColor = AntiqueWhite;
+            }
+            if (button1.BackColor == AntiqueWhite)
+            {
+                button1.BackColor = Red;
+            }
+            else if (button1.BackColor == Red)
+            {
+                button1.BackColor = AntiqueWhite;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (button6.BackColor == AntiqueWhite)
+            {
+                button6.BackColor = Red;
+            }
+            else if (button6.BackColor == Red)
+            {
+                button6.BackColor = AntiqueWhite;
+            }
+            if (button1.BackColor == AntiqueWhite)
+            {
+                button1.BackColor = Red;
+            }
+            else if (button1.BackColor == Red)
+            {
+                button1.BackColor = AntiqueWhite;
+            }
+            if (button2.BackColor == AntiqueWhite)
+            {
+                button2.BackColor = Red;
+            }
+            else if (button2.BackColor == Red)
+            {
+                button2.BackColor = AntiqueWhite;
+            }
+        }
+
+
+
+    }
+}
